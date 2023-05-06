c624d228435b8d5fbae454d4988b54170c189cec
Владислав Тришкин <trishkin.vlad197@yandex.ru>
2023-05-06 16:51:33 +0300

    Add docstring

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from random import randint
 
+from graphic_arts.start_game_banner import run_screensaver
+
 
 def attack(char_name: str, char_class: str) -> str:
+    """Функция генерирует количество очков атаки.
+    в зависимости от выбранного типа персонажа
+    и возвращает строковое сообщение о проведённой атаке.
+    """
     if char_class == 'warrior':
         return (f'{char_name} нанёс противнику урон, равный '
                 f'{5 + randint(3, 5)}')
@@ -15,6 +21,10 @@ def attack(char_name: str, char_class: str) -> str:
 
 
 def defence(char_name: str, char_class: str) -> str:
+    """Фунцкия генерирует количество очков защиты.
+    в зависимости от выбранного типа персонажа
+    и возвращает строковое сообщение о выполненном блокировании атаки.
+    """
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} ед. урона')
     if char_class == 'mage':
@@ -25,6 +35,9 @@ def defence(char_name: str, char_class: str) -> str:
 
 
 def special(char_name: str, char_class: str) -> str:
+    """Функция принимает на вход имя персонажа и его класс.
+    и возвращает строку, описывающую специальное умение персонажа.
+    """
     if char_class == 'warrior':
         return (f'{char_name} применил специальное умение '
                 f'«Выносливость {80 + 25}»')
@@ -36,6 +49,10 @@ def special(char_name: str, char_class: str) -> str:
 
 
 def start_training(char_name: str, char_class: str) -> str:
+    """Фунция запускает цикл тренировки навыков персонажа.
+    В качестве параметров она получает
+    введённое игроком имя персонажа и выбранный тип персонажа.
+    """
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — великий мастер ближнего боя.')
     if char_class == 'mage':
@@ -60,6 +77,10 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class() -> str:
+    """Функция запрашивает у пользователя выбор класса персонажа.
+    выводит описание выбранных классов
+    и возвращает строку с выбранным классом.
+    """
     approve_choice: str = ''
     char_class: str = ''
     while approve_choice != 'y':
@@ -81,15 +102,14 @@ def choice_char_class() -> str:
     return char_class
 
 
-def main():
+if __name__ == '__main__':
+    run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
-
-
-char_name: str = input('...назови себя: ')
-print(f'Здравствуй, {char_name}! '
-      'Сейчас твоя выносливость — 80, атака — 5 и защита — 10.')
-print('Ты можешь выбрать один из трёх путей силы:')
-print('Воитель, Маг, Лекарь')
-char_class: str = choice_char_class()
-print(start_training(char_name, char_class))
+    char_name: str = input('...назови себя: ')
+    print(f'Здравствуй, {char_name}! '
+          'Сейчас твоя выносливость — 80, атака — 5 и защита — 10.')
+    print('Ты можешь выбрать один из трёх путей силы:')
+    print('Воитель, Маг, Лекарь')
+    char_class: str = choice_char_class()
+    print(start_training(char_name, char_class))
